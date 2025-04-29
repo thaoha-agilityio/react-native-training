15e895fb42a1e30dd212b948bfaf896b1207e9bb
Thao Ha <thao.ha@asnet.com.vn>
2025-04-29 17:27:40 +0700

    feat: remove code redundant

--- e-commerce/app/+not-found.tsx
+++ e-commerce/app/+not-found.tsx
@@ -1,32 +1,17 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Stack } from 'expo-router';
+import { StyleSheet, Text } from 'react-native';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+      <Text style={styles.link}>This screen doesn't exist</Text>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
   link: {
-    marginTop: 15,
-    paddingVertical: 15,
+    color: '#fff',
   },
 });
---
+++ e-commerce/components/icons/HeartIcon.tsx
@@ -0,0 +1,13 @@
+import Svg, { Path, SvgProps } from 'react-native-svg';
+
+export const HeartIcon = ({ width = 24, height = 24, ...rest }: SvgProps) => (
+  <Svg width={width} height={height} fill="none" {...rest}>
+    <Path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.501 5.501 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78Z"
+    />
+  </Svg>
+);
